1bf56918970f198f4782ec0655e75ff450bae94a
Katie Overton <koverto1@jaguarlandrover.com>
2022-01-31 11:02:50 +0000

    Creating the flask instance and set default configurations within the __init__.py file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,27 @@
 import os
 
 from flask import Flask
+
+
+def create_website(test_config=None):
+    resource_board = Flask(__name__, instance_relative_config=True)
+    resource_board.config.from_mapping(
+        SECRET_KEY='develop',
+        DATABASE=os.path.join(resource_board.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        resource_board.config.from_pyfile('config.py', silent=True)
+    else:
+        resource_board.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(resource_board.instance_path)
+    except OSError:
+        pass
+
+    @resource_board.route('/test_open')
+    def hello():
+        return 'Welcome to the resource board!'
+
+    return resource_board
