c7e74fff5f3b69e8c5d9667a62d8e3ba29bee9fc
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-10 09:32:33 +0000

    Authentication python file containing function for an administrative user to log in

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,3 +8,27 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/admin', methods=('GET', 'POST'))
+def admin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # role = request.form['role']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            if username == "admin" and password == "adminpassword":
+                print('Successful Login')
+                return redirect(url_for('auth.register'))
+            else:
+                error = 'Incorrect administrative credentials. Please try again.'
+        flash(error)
+
+    return render_template('auth/admin.html')
