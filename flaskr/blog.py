4c95d246637f0921174baee883eeceb2979f7b50
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-11 10:04:48 +0000

    Resource board python file with initial functionality to collect information about the user and resource to be included in the student view of the resource board html file called student_index.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -19,3 +19,14 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/student_index')
+def student_index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, body2, module, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/student_index.html', posts=posts)
