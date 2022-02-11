61dfbdea45e4e6cb37dac4431a209705e76d1e3a
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-11 10:05:55 +0000

    Resource board python file containing functionality called 'create' to allow the user to upload a new resource

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -30,3 +30,68 @@ def student_index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/student_index.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        print(title)
+        print(type(title))
+        body = request.form['body']
+        rating = request.form['rating']
+        print(rating)
+        topic = request.form['topic']
+        print(topic)
+        error = None
+
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+            print(os.getcwd())
+
+            file.save(os.path.join('flaskr\\uploads\\', filename))
+            print("saved file successfully")
+      # send file name as parameter to downlad
+            # return redirect('/downloadfile/' + filename)
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id, body2)'
+                ' VALUES (?, ?, ?, ?)',
+                (title, filename, g.user['id'], body)
+            )
+            db.commit()
+            db = get_db()
+            error = None
+            body_post = db.execute(
+                'SELECT body2 FROM post WHERE title = ?', (title,)
+            ).fetchone()
+            body_post2 = body_post[0]
+            print('this is it:' + body_post2)
+
+            # print(post[1])
+            # print(post[2])
+            # print(post[3])
+            # print(post[4])
+            # print(post[5])
+
+            # return redirect(url_for('blog.index'))
+            session[body_post2] = body_post2
+            return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
