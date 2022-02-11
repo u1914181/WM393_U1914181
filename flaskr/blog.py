36935977085dc8f7eb7d8b128d5566dc56037f53
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-11 09:58:31 +0000

    Resource board python file with initial functionality to collect information about the user and resource to be included in the resource board html file called index.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,3 +6,14 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 from werkzeug.utils import secure_filename
 import os
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, body2, module, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
