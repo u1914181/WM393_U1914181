2aaa8c3cd05b82d5d964b5d6abca31372e32c88a
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-13 20:40:14 +0000

    __init__.py file with commented code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,22 @@
+'''
+Module imports:
+os provides functions for creating and removing a directory and retrieving its contents.
+Flask is a web framework that allows for the easy development of web applications.
+'''
 import os
 
 from flask import Flask
+
+'''
+create_app is the application factory function.
+The flask instance is created by app = Flask(__name__, instance_relative_config=True).
+__name__ is the current python module.
+The instance folder is located outside of the flaskr package and holds the database instance.
+app.config.from_mapping() sets some default configurations such as the secret_key and the database (path to db).
+app.config.from_pyfile() overrides the default configurations, should this be needed in deployment.
+os.makedirs() ensures the instance folder exists.
+app.route() creates a simple route to test the app is working.
+'''
 
 
 def create_app(test_config=None):
@@ -28,18 +44,20 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
+    # Imports the SQL database.
     from . import db
     db.init_app(app)
 
     from . import auth
+    # Registers the blueprint.
     app.register_blueprint(auth.bp)
 
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+    # All files to upload to the application are saved within the uploads/ folder.
     UPLOAD_FOLDER = 'uploads/'
 
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+    # returns the application to the server.
     return app
