ccffd0cc0e3737175f7d91e155d7828feb3055c5
Katie Overton <koverto1@jaguarlandrover.com>
2022-02-13 20:37:06 +0000

    Python database file with commented code

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,8 +1,15 @@
+# SQL and flask module imports.
 import sqlite3
 
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+
+'''
+g is used to store data that is relevant for the duration of the request.
+current_app directs to the flask application processing the request.
+sqlite3.connect() establishes the connection to the sql file.
+'''
 
 
 def get_db():
@@ -11,9 +18,11 @@ def get_db():
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
+        g.db.row_factory = sqlite3.Row  # Row means return rows.
 
     return g.db
+
+# close_db closes the database connection.
 
 
 def close_db(e=None):
@@ -22,10 +31,12 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+# init_db initialises the database ready for use.
+
 
 def init_db():
     db = get_db()
-
+    # schema.sql is the name of the sql database created for this project.
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
@@ -33,11 +44,11 @@ def init_db():
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
     init_db()
+    # Message printed to the terminal.
     click.echo('Initialized the database.')
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+    app.teardown_appcontext(close_db) # flask will call this function after returning a response.
+    app.cli.add_command(init_db_command) # adds new command that can be called using flask command.
